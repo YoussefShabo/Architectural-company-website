1748b2de535d4d1a969834aa662bb3c984719e26
Youssef Shabo <youssefshabo@Youssefs-MacBook-Pro.local>
2023-03-14 17:35:34 -0600

    updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Architectural-company-website
 the second project for GA Bootcamp
+
+
+background grey color: #737373
+golden color: #ffbd59
+
+- Logo designed on Canva.com
+- map was embedded from google maps
+- Boostrap was used for the main container to give margins
+- Skeleton was used to style text
+
+Basic app structure is based on the CRUD app examples from previous lectures and labs.
---
+++ models/projects.js
@@ -0,0 +1,97 @@
+
+const projects = [
+{
+    name: `XIONG'AN BRIDGES`,
+    location: 'Hebei, China',
+    date: 2020,
+    typology: 'Infrastructure',
+    description: `Connecting, therefore is a keyword, is a fundamental action to ensure the well-functioning of the city.
+
+    This sequence of bridges will do more than just connect vital functions; they will be platforms to support the liveliness of the city by bringing the citizens together by the waterfront.
+    
+    This will have to be done bearing in mind that these bridges will also have to operate as sustainable performative devices that will improve the user experience.
+    
+    Serving as large planters for all types of vegetation to bloom and flourish, improving green parameters such as air quality without forgetting to generate the minimum footprint to preserve the natural ecosystem that runs below them.
+    
+    We decide to look at the bridge as a symbol of transition and universal understanding.
+    
+    The design will evoke a walk through the mountains, the thrill of crossing an abyss… suspended up in the air. The key element will be AIR.`,
+    image1: `./XIONG'AN BRIDGES/01-02-03_aerial-view_lr.jpeg`,
+    image2: `./XIONG'AN BRIDGES/04-05-06_aerial-view_lr.jpeg`,
+    image3: `./XIONG'AN BRIDGES/07-08-09-14_aerial-view.jpeg`,
+    image4: `./XIONG'AN BRIDGES/10-11-12-13_aerial-view_lr.jpeg`,
+    image5: `./XIONG'AN BRIDGES/10-11-12-13-general-concept_ENRIC-MIRALLES_web.jpeg`,
+    image6: `./XIONG'AN BRIDGES/BRIDGE-01_human-view_lr.jpeg`,
+    image7: `./XIONG'AN BRIDGES/BRIDGE-07_human-view_lr.jpeg`,
+},
+{
+    name: `CENTURY SQUARE`,
+    location: 'Shanghai, China',
+    date: 2020,
+    typology: 'Urban Spaces',
+    description: `The new design for the very significant “Century Square” at East Nanjing Road in Shanghai, opens like a shell and shows the splendour of its 'pearl', a shining kaleidoscope which reflects the new green urban scenery.
+
+    “Century Square” has always been related to shows and events, positioned in a strategic connection between “Peoples Square” and “The Bund”, and characterized by its special ambience of incredible night lights and commercial presentations.
+    
+    Our project gives a homage to the origin of the word Shanghai, which means 'upon the sea'. We convert this very keyspace into a new urban living room, inspired in colours and shape by the sea and the harbour. The new paving represents an undersea world. This pavement becomes illuminated at night by glittering lights.
+    
+    We decide to renature this city centre, introducing green spaces to improve the urban microclimate in consideration of the ecological consciousness. Lights and audiovisuals enhance this new green open space at night.
+    
+    We merge the newly introduced ecological aspects with the heritage of the existing buildings and functional requirements. Shows of different natures will happen here, and the facades on Nanjing Road participate by hosting lights and screens, beautifully redesign.
+    
+    Our new kaleidoscope building is the new heart of the century square, its 'pearl'. It hosts spectacular shows, and it is a show in itself.
+    
+    The facades reflect like a water surface, by fragmenting and recomposing the lights and images of the surroundings, with its colourful paving and greenery, and with its commercials and historical buildings, Billboards, etc…
+    
+    The south side of the “Century Square” is organized by a topographic element: green hills, interwoven with the existing trees, support a wooden grandstand which can be used in many different ways; giving space to different functions such as cafeterias and shops, creating a new urban eco-living room.
+    
+    Green terraces and steps will allow people to sit at different levels, surrounded by trees and leaves, overviewing the vitality of the square. Different events take place here: product presentations, exhibitions, markets, culture and theatre events, adapting each time the scenario.
+    
+    Inside the kaleidoscope, small rooms invite the public to discover the square, the sky, the spirit of the city in between plants and mirrors.
+    
+    The renewed “Century Square” will become the highlight of East Nanjing Road, a new venue to experience the natural beauty of Shanghai.`,
+    image1: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Aerial-view-01_web.jpeg`,
+    image2: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Aerial-view-02_web.jpeg`,
+    image3: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Collage-River_web.jpeg`,
+    image4: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Daylight-view-01_web.jpeg`,
+    image5: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Masterplan-1_web.jpeg`,
+    image6: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Night-aerial-view_crop.jpeg`,
+    image7: `./RENEWAL OF CENTURY SQUARE IN EAST NANJING ROAD/Century-Square_Situation_web.jpeg`,
+},
+{
+    name: `A SCHOOL FOR COLOMBO`,
+    location: 'Colombo, Sri Lanka',
+    date: 2020,
+    typology: 'Educational Spaces',
+    description: `The new design for the very significant “Century Square” at East Nanjing Road in Shanghai, opens like a shell and shows the splendour of its 'pearl', a shining kaleidoscope which reflects the new green urban scenery.
+
+    “Century Square” has always been related to shows and events, positioned in a strategic connection between “Peoples Square” and “The Bund”, and characterized by its special ambience of incredible night lights and commercial presentations.
+    
+    Our project gives a homage to the origin of the word Shanghai, which means 'upon the sea'. We convert this very keyspace into a new urban living room, inspired in colours and shape by the sea and the harbour. The new paving represents an undersea world. This pavement becomes illuminated at night by glittering lights.
+    
+    We decide to renature this city centre, introducing green spaces to improve the urban microclimate in consideration of the ecological consciousness. Lights and audiovisuals enhance this new green open space at night.
+    
+    We merge the newly introduced ecological aspects with the heritage of the existing buildings and functional requirements. Shows of different natures will happen here, and the facades on Nanjing Road participate by hosting lights and screens, beautifully redesign.
+    
+    Our new kaleidoscope building is the new heart of the century square, its 'pearl'. It hosts spectacular shows, and it is a show in itself.
+    
+    The facades reflect like a water surface, by fragmenting and recomposing the lights and images of the surroundings, with its colourful paving and greenery, and with its commercials and historical buildings, Billboards, etc…
+    
+    The south side of the “Century Square” is organized by a topographic element: green hills, interwoven with the existing trees, support a wooden grandstand which can be used in many different ways; giving space to different functions such as cafeterias and shops, creating a new urban eco-living room.
+    
+    Green terraces and steps will allow people to sit at different levels, surrounded by trees and leaves, overviewing the vitality of the square. Different events take place here: product presentations, exhibitions, markets, culture and theatre events, adapting each time the scenario.
+    
+    Inside the kaleidoscope, small rooms invite the public to discover the square, the sky, the spirit of the city in between plants and mirrors.
+    
+    The renewed “Century Square” will become the highlight of East Nanjing Road, a new venue to experience the natural beauty of Shanghai.`,
+    image1: `./school/school.jpg`,
+    image2: `./school/oasis.jpg`,
+    image3: `./school/corner 1.jpg`,
+    image4: `./school/auditorium test4.jpg`,
+    image5: `././school/Cafeteria interior4.jpg`,
+    image6: `./school/classrom 1.jpg`,
+    image7: `./school/library interior3.jpg`,
+},
+]
+
+module.exports = projects
